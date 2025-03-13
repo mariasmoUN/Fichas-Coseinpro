700486687d712e8382e553ca3a5060cc0d051246
Mariana Arias <mariasmo@unal.edu.co>
2025-03-13 10:05:05 -0500

    Actualización índices por incorporación de estado

--- script.js
+++ script.js
@@ -11,35 +11,33 @@ function value() {
 function resumir() {
   let texto = document.getElementById("general_text").value;
 
-  //const input = `02/03/2025	192102296	1603103	15666-3266556		Arriendo	Apartamento	Medellín	Calasanz	Calasanz parte alta	Cerros del Escorial	Calle 51 # 82 190	2.300.000			523	80	80	5	3	3	Integral	EPM	Salón - Comedor	Calentador, Closet, Zona de ropas, Alcoba de servicio, ventanal	Cubierto	1	Cerámica	Gimnasio, Juegos infantiles, Parqueadero para visitantes, Portería, Turco, Zonas verdes	Cerca a la calle colombia, la 80, parroquias, parques, colegios, tiendas, supermercados y buenas rutas de transporte público.	Propietaria							Apartamento para arriendo en Calasanz, con excelente ubicación cerca a la calle Colombia, la 80, parroquias, parques, colegios, tiendas, supermercados y buenas rutas de transporte público. Ubicado en un piso 5, el apartamento cuenta con un área de 80 m² aproximados y consta de 3 alcobas más una alcoba de servicio, 3 baños, closets, salón comedor, ventanal, cocina integral, calentador, zona de ropas y parqueadero cubierto. En unidad cerrada que cuenta con juegos infantiles, gimnasio, turco, parqueadero para visitantes, zonas verdes y portería 24 horas.`;
-
   const fields = texto.split("\t");
 
   // Asignar campos por índice para mayor claridad
-  const fechaIngreso = fields[1];
-  const fr = fields[2];
-  const eu = fields[3];
-  const cc = fields[4];
-  const m2 = fields[5];
-  const tipoOperacion = fields[6]; // Arriendo
-  const tipoPropiedad = fields[7]; // Apartamento
-  const ciudad = fields[8];
-  const barrio = `${fields[9]} - ${fields[10]}`;
-  const direccion = `${fields[11]} - ${fields[12]}`; // Calle 51 # 82 190
-  const canon = fields[13];
-  const apto = fields[16]; // 523
-  const area = fields[17];
-  const areatotal = fields[18];
-  const estrato = fields[19];
-  const alcobas = fields[20];
-  const banos = fields[21];
-  const cocina = `${fields[22]} con gas de ${fields[23]}`;
-  const ambientes = fields[24];
-  const detalles = fields[25]; // Calentador, Closet, Zona de ropas, Alcoba de servicio, ventanal
-  const parqueadero = `${fields[27]} ${fields[26].toLowerCase()}`; // 1 cubierto
-  const piso = fields[28];
-  const areascomunes = fields[29];
-  const cercanias = fields[30]; // Puedes ajustarlo si quieres usar el campo 30
+  const fechaIngreso = fields[2];
+  const fr = fields[3];
+  const eu = fields[4];
+  const cc = fields[5];
+  const m2 = fields[6];
+  const tipoOperacion = fields[7]; // Arriendo
+  const tipoPropiedad = fields[8]; // Apartamento
+  const ciudad = fields[9];
+  const barrio = `${fields[10]} - ${fields[11]}`;
+  const direccion = `${fields[12]} - ${fields[13]}`; // Calle 51 # 82 190
+  const canon = fields[14];
+  const apto = fields[17]; // 523
+  const area = fields[18];
+  const areatotal = fields[19];
+  const estrato = fields[20];
+  const alcobas = fields[21];
+  const banos = fields[22];
+  const cocina = `${fields[23]} con gas de ${fields[24]}`;
+  const ambientes = fields[25];
+  const detalles = fields[26]; // Calentador, Closet, Zona de ropas, Alcoba de servicio, ventanal
+  const parqueadero = `${fields[28]} ${fields[27].toLowerCase()}`; // 1 cubierto
+  const piso = fields[29];
+  const areascomunes = fields[30];
+  const cercanias = fields[31]; // Puedes ajustarlo si quieres usar el campo 30
 
   // Construcción del resumen
   const fichaResumen = `
